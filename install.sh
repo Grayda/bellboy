98f05cb8d0078118031fc955b4554360e26dc06d
David Gray <grayda@solidinc.org>
2015-09-07 20:32:35 +1000

    Directory checks and rc.local tweaks
    Should now run, as nodemon has absolute path

--- install.sh
+++ install.sh
@@ -8,13 +8,26 @@ echo "Installing audio dependency for Bellboy.."
 sudo apt-get install libasound2-dev # For audio support
 echo "CDing into /home/pi.."
 cd /home/pi
-echo "Cloning Bellboy into /home/pi.."
-git clone -b beta http://github.com/Grayda/bellboy.git
-cd /home/pi/bellboy
+if [ -d "/home/pi/bellboy" ] ;
+  then
+    echo "Cloning Bellboy into /home/pi.."
+    git clone -b beta http://github.com/Grayda/bellboy.git
+    cd /home/pi/bellboy
+  else
+    echo "Bellboy already cloned. Updating.."
+    cd /home/pi/bellboy
+    git pull origin beta
+  fi
+
 echo "Installing nodemon globally.."
 sudo npm install -g nodemon
 echo "Installing other dependencies.."
 npm install
-echo "Adding script to startup.."
-sudo sed -i -e '$i \nodemon /home/pi/bellboy/index.js &\n' /etc/rc.local
+if grep -q bellboy /etc/rc.local ;
+  then
+    echo "Adding script to startup.."
+    sudo sed -i -e '$i \/usr/local/bin/nodemon /home/pi/bellboy/index.js &\n' /etc/rc.local
+  else
+    echo "bellboy already in startup. Skipping.."
+  fi
 read -p "Press [Enter] key to reboot.."
