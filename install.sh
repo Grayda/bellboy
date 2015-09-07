dcd5b9bc95ade6721964ea56285ad663292930a7
David Gray <grayda@solidinc.org>
2015-09-07 20:56:50 +1000

    Switch back to init.d startup
    Less hacky that way.

--- install.sh
+++ install.sh
@@ -8,26 +8,25 @@ echo "Installing audio dependency for Bellboy.."
 sudo apt-get install libasound2-dev # For audio support
 echo "CDing into /home/pi.."
 cd /home/pi
-if [ -d "/home/pi/bellboy/" ] ;
+if [ -d "/home/pi/bellboy/" ];
   then
-    echo "Cloning Bellboy into /home/pi.."
-    git clone -b beta http://github.com/Grayda/bellboy.git
-    cd /home/pi/bellboy
-  else
     echo "Bellboy already cloned. Updating.."
     cd /home/pi/bellboy
     git pull origin beta
+  else
+    echo "Cloning Bellboy into /home/pi.."
+    git clone -b beta http://github.com/Grayda/bellboy.git
+    cd /home/pi/bellboy
   fi
 
 echo "Installing nodemon globally.."
 sudo npm install -g nodemon
 echo "Installing other dependencies.."
 npm install
-if grep -q "bellboy" "/etc/rc.local" ;
-  then
-    echo "bellboy already in startup. Skipping.."
-  else
-    echo "Adding script to startup.."
-    sudo sed -i -e '$i \/usr/local/bin/nodemon /home/pi/bellboy/index.js &\n' /etc/rc.local
-  fi
+echo "Copying startup script to /etc/init.d.."
+cp bellboy.sh /etc/init.d/
+echo "Making the script executable"
+sudo chmod 755 /etc/init.d/bellboy.sh
+echo "Registering changes.."
+sudo update-rc.d bellboy.sh defaults
 read -p "Press [Enter] key to reboot.."
