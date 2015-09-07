2ed084a1037e04875eb7993d38d428be6cb06fcf
David Gray <grayda@solidinc.org>
2015-09-07 17:31:24 +1000

    Install script bugfixes
    Also changed the log filename so git ignores it when committing.

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ echo "Downloading Node PPA.."
 curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
 echo "Installing Node.."
 sudo apt-get install -y nodejs
-echo "Version of Node now installed:"
-node –v # For version checking
 echo "Installing audio dependency for Bellboy.."
 sudo apt-get install libasound2-dev # For audio support
 echo "CDing into /home/pi.."
@@ -18,8 +16,8 @@ sudo npm install -g nodemon
 echo "Installing other dependencies.."
 npm install
 echo "Copying startup script into /etc/init.d"
-sudo cp bellboy.sh /etc/init.d/
+sudo cp /home/pi/bellboy/bellboy.sh /etc/init.d/
 echo "Making the startup script executable"
 sudo chmod 755 /etc/init.d/bellboy.sh
-echo "Rebooting.."
+read -p "Press [Enter] key to reboot..."
 sudo reboot
