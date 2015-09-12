63cea4adc0dd6d0ba87ce910d5b13a40eeab59eb
David Gray <grayda@solidinc.org>
2015-09-12 14:05:17 +1000

    Added option to disable entire bell system, plus theme tweaks

--- index.js
+++ index.js
@@ -17,18 +17,13 @@ start()
 
 function start() {
   loadSettings()
-  c()
   c("Loading bells..")
-  c()
-    // loadBells() also calls showTable()
   loadBells()
-  c()
 
-  // Start the web server
-  if (config.Server === true) {
+  // Start the web server if it's enabled
+  if (config.Server) {
     startServer()
   }
-
 }
 
 // Play the audio file
@@ -75,9 +70,9 @@ function sendRawEmail(from, to, subject, body, options) {
       head: ['green', 'bold']
     }
   });
+
   table.push(
     ["Server", config.Email.Server], ["From", from], ["To", to], ["Subject", tSubject], ["Body", tBody], ["Time", moment().format(config.DateFormat)]
-
   );
 
   var email = require("emailjs");
@@ -95,11 +90,8 @@ function sendRawEmail(from, to, subject, body, options) {
     to: to,
     subject: tSubject
   }, function(err, message) {
-    c(err || message);
+    if(err) { c(err) }
   });
-
-  // Add our details to it. / Server From
-
 
   c(table.toString())
 
@@ -112,7 +104,165 @@ function startServer() {
   var server = http.createServer(function(request, response) {
     dispatcher.dispatch(request, response);
   });
-  // Ask to update the
+
+  // ===============================================================================================
+  // Call to the root
+  dispatcher.onGet("/", function(req, res) {
+    file = fs.readFileSync("./web/index.html").toString()
+
+    var options = {
+      items: bells.Bells,
+      bellsenabled: bells.Bells.Enabled,
+      Date: moment().format(config.DateFormat),
+      nextJob: nextJob(),
+      cron: function(cron) {
+        return cronToDate(cron)
+      },
+      query: req,
+      filename: "./web/header.html"
+    }
+    res.end(ejs.render(file, options))
+
+  });
+  // ===============================================================================================
+  // We've requested an image. Needs to be sent in binary
+  dispatcher.beforeFilter(/\.jpg|\.png|\.gif|\.bmp\.ttf|\.woff/g, function(req, res) {
+    file = fs.readFileSync("./web" + url.parse(req.url).pathname)
+    console.log("Binary")
+    res.end(file, 'binary')
+  })
+
+  // ===============================================================================================
+  // We've requested a CSS file. Pass it the WebTheme from our config file
+  dispatcher.beforeFilter(/\.less/g, function(req, res) {
+    var options = {
+      theme: config.WebTheme,
+      filename: "./web/header.html"
+    }
+
+    file = fs.readFileSync("./web" + url.parse(req.url).pathname).toString()
+    less.render(ejs.render(file, options), function(e, output) {
+      res.end(output.css)
+    })
+
+  })
+
+  // ===============================================================================================
+  // We've requested a CSS file. Pass it the WebTheme from our config file
+  dispatcher.beforeFilter(/\.css/g, function(req, res) {
+    var options = {
+      theme: config.WebTheme,
+      filename: "./web/header.html",
+      nextJob: nextJob(),
+      cron: function(cron) {
+        return cronToDate(cron)
+      }
+    }
+
+    file = fs.readFileSync("./web" + url.parse(req.url).pathname).toString()
+    res.end(ejs.render(file, options))
+
+  })
+
+  // ===============================================================================================
+  // Not yet complete.
+  dispatcher.onGet("/add.html", function(req, res) {
+    file = fs.readFileSync("./web" + url.parse(req.url).pathname).toString()
+    var options = {
+      items: bells.Bells,
+      Date: moment().format(config.DateFormat),
+      query: req,
+      filename: "./web/header.html",
+      nextJob: nextJob(),
+      cron: function(cron) {
+        return cronToDate(cron)
+      }
+    }
+    res.end(ejs.render(file, options))
+
+  })
+
+  // ===============================================================================================
+  // View the logs
+  dispatcher.onGet("/logs.html", function(req, res) {
+    try {
+
+      file = fs.readFileSync("./web/logs.html").toString()
+
+      var options = {
+        Date: moment().format(config.DateFormat),
+        logs: fs.readFileSync("bellboy.log").toString().replace("\n", "<br />"),
+        nextJob: nextJob(),
+        cron: function(cron) {
+          return cronToDate(cron)
+        },
+        filename: "./web/header.html"
+      }
+      res.end(ejs.render(file, options))
+    } catch (ex) {
+      console.log("Error parsing log: " + ex)
+    }
+  })
+
+  // ===============================================================================================
+  // Reload bells and settings
+
+  dispatcher.onGet("/reload.html", function(req, res) {
+    loadBells()
+    loadSettings()
+
+    var options = {
+      Date: moment().format(config.DateFormat),
+      filename: "./web/header.html",
+      nextJob: nextJob(),
+      cron: function(cron) {
+        return cronToDate(cron)
+      }
+    }
+
+    file = fs.readFileSync("./web/reload.html").toString()
+    res.end(ejs.render(file, options))
+  });
+
+  // ===============================================================================================
+  // When we're switching a bell on or off
+  dispatcher.onGet("/toggle.html", function(req, res) {
+
+      var state = (req.params.state === "true")
+      toggleBell(req.params.id, state, function(success) {
+        if (success == false) {
+          c("State NOT updated, as the job is locked")
+        } else {
+          showTable()
+          saveBells()
+        }
+
+        // Grab the toggle.html file for updating
+        file = fs.readFileSync("./web" + url.parse(req.url).pathname).toString()
+          // Options the template will have access to
+        var options = {
+          item: bells.Bells[req.params.id],
+          Date: moment().format(config.DateFormat),
+          nextJob: nextJob(),
+          cron: function(cron) {
+            return cronToDate(cron)
+          },
+          state: state,
+          filename: "./web/header.html"
+        }
+
+        // Render the template and write it to our waiting client.
+        res.end(ejs.render(file, options))
+      })
+
+
+
+  })
+
+
+  // ===============================================================================================
+  // Ask to update the app using git pull
+  // TO-DO: Make sure bells don't get overwritten by git pull!
   dispatcher.onGet("/update.html", function(req, res) {
     var exec = require('child_process').exec
     var strStdout, branch
@@ -135,138 +285,14 @@ function startServer() {
       var options = {
         Date: moment().format(config.DateFormat),
         status: stdout || stderr,
+        nextJob: nextJob(),
+        cron: function(cron) {
+          return cronToDate(cron)
+        },
         filename: "./web/header.html"
       }
       res.end(ejs.render(file, options))
     })
-  })
-
-  dispatcher.onGet("/logs.html", function(req, res) {
-    try {
-
-      file = fs.readFileSync("./web/logs.html").toString()
-
-      var options = {
-        Date: moment().format(config.DateFormat),
-        logs: fs.readFileSync("bellboy.log").toString().replace("\n", "<br />"),
-        filename: "./web/header.html"
-      }
-      res.end(ejs.render(file, options))
-    } catch (ex) {
-      console.log("Error parsing log: " + ex)
-    }
-  })
-
-  // When we're switching a bell on or off
-  dispatcher.onGet("/toggle.html", function(req, res) {
-    try {
-      // Force our state to be a boolean
-      var state = (req.params.state === "true")
-        // Toggle the bell. Last param is a callback.
-      toggleBell(req.params.id, state, function(success) {
-        // If change not successful, due to job being locked
-        if (success == false) {
-          c("State NOT updated, as the job is locked")
-        } else {
-          // Show an updated table of our jobs
-          showTable()
-            // Save the updated bells
-          saveBells()
-        }
-
-        // Grab the toggle.html file for updating
-        file = fs.readFileSync("./web" + url.parse(req.url).pathname).toString()
-          // Options the template will have access to
-        var options = {
-          item: bells.Bells[req.params.id],
-          Date: moment().format(config.DateFormat),
-          state: state,
-          filename: "./web/header.html"
-        }
-
-        // Render the template and write it to our waiting client.
-        res.end(ejs.render(file, options))
-
-      })
-    } catch (ex) {
-      res.end("Cannot find file! " + ex)
-    }
-  })
-
-  // We've requested an image. Needs to be sent in binary
-  dispatcher.beforeFilter(/\.jpg|\.png|\.gif|\.bmp\.ttf|\.woff/g, function(req, res) {
-    file = fs.readFileSync("./web" + url.parse(req.url).pathname)
-    console.log("Binary")
-    res.end(file, 'binary')
-  })
-
-  // We've requested a CSS file. Pass it the WebTheme from our config file
-  dispatcher.beforeFilter(/\.less/g, function(req, res) {
-    var options = {
-      theme: config.WebTheme,
-      filename: "./web/header.html"
-    }
-
-    file = fs.readFileSync("./web" + url.parse(req.url).pathname).toString()
-    less.render(ejs.render(file, options), function(e, output) {
-      res.end(output.css)
-    })
-
-  })
-
-  // We've requested a CSS file. Pass it the WebTheme from our config file
-  dispatcher.beforeFilter(/\.css/g, function(req, res) {
-    var options = {
-      theme: config.WebTheme,
-      filename: "./web/header.html"
-    }
-
-    file = fs.readFileSync("./web" + url.parse(req.url).pathname).toString()
-    res.end(ejs.render(file, options))
-
-  })
-
-
-  // Call to the root
-  dispatcher.onGet("/", function(req, res) {
-    file = fs.readFileSync("./web/index.html").toString()
-
-    var options = {
-      items: bells.Bells,
-      Date: moment().format(config.DateFormat),
-      nextJob: nextJob(),
-      cron: function(cron) { return cronToDate(cron) },
-      query: req,
-      filename: "./web/header.html"
-    }
-    res.end(ejs.render(file, options))
-
-  });
-
-  dispatcher.onGet("/reload.html", function(req, res) {
-    loadBells()
-    loadSettings()
-
-    var options = {
-      Date: moment().format(config.DateFormat),
-      filename: "./web/header.html"
-    }
-
-    file = fs.readFileSync("./web/reload.html").toString()
-    res.end(ejs.render(file, options))
-  });
-
-  // Not yet complete.
-  dispatcher.onGet("/add.html", function(req, res) {
-    file = fs.readFileSync("./web" + url.parse(req.url).pathname).toString()
-    var options = {
-      items: bells.Bells,
-      Date: moment().format(config.DateFormat),
-      query: req,
-      filename: "./web/header.html"
-    }
-    res.end(ejs.render(file, options))
-
   })
 
   // After our setup, set our server to listen
@@ -278,6 +304,7 @@ function startServer() {
 
 // Sets a bell to the specified state. Supports a callback so you know when it's done.
 function toggleBell(bell, state, callback) {
+
   // Force "locked" to be a boolean. Need to check this line in other functions, as it could be security risk
   var locked = (bells.Bells[bell].Locked === "true")
     // If the job isn't locked (meaning it can be changed via the web UI)
@@ -285,11 +312,15 @@ function toggleBell(bell, state, callback) {
     // If state is going to be true, start the job
     if (state === true) {
       c("Starting Cron job for " + bell)
-      jobs[bell].start()
+      if (bell != "_all") {
+        jobs[bell].start()
+      }
     } else {
       // Stop the job if we're disabling it
       c("Stopping Cron job for " + bell)
-      jobs[bell].stop()
+      if (bell != "_all") {
+        jobs[bell].stop()
+      }
     }
     // Set the bell. It's your responsibility to call saveBells() later
     bells.Bells[bell].Enabled = state
@@ -316,7 +347,7 @@ function loadSettings() {
 function loadBells() {
   bells = JSON.parse(fs.readFileSync(config.BellFile, 'utf8'));
 
-  if (bells.Enabled === false) {
+  if (bells.Bells["_all"].Enabled === false) {
     c("=======================================================================")
     c("Bells are currently DISABLED. No bells will ring until they are enabled")
     c("=======================================================================")
@@ -326,6 +357,7 @@ function loadBells() {
   // Loop through all the bells we have
   // Because bells.Bells uses a string based key, we have to do it this way.
   Object.keys(bells.Bells).forEach(function(item) {
+    if(item === "_all") { return }
     try {
 
 
@@ -336,7 +368,7 @@ function loadBells() {
     // Create a new Cron job at the specified .Time (a Cron expression)
     jobs[item] = new CronJob(bells.Bells[item].Time, function() {
       // If bells are DISABLED, just return. Don't process anything.
-      if (bells.Enabled === false) {
+      if (bells.Bells["_all"].Enabled === false) {
         c("=======================================================================")
         c("Bells are currently DISABLED. No bells will ring until they are enabled")
         c("=======================================================================")
@@ -364,7 +396,10 @@ function loadBells() {
 
 
 function cronToDate(cron) {
-  return moment(parser.parseExpression(cron).next()).calendar()
+  var details = []
+  details["calendar"] = moment(parser.parseExpression(cron).next()).calendar()
+  details["to"] = moment().to(parser.parseExpression(cron).next())
+  return details
 }
 
 function nextJob() {
@@ -373,6 +408,9 @@ function nextJob() {
     // Because the diff is a negative number (smaller as it approaches "now") and we're lookiung for a number > the smallest, we set this insanely low
   results["diff"] = -9999999999999999999999999999
   Object.keys(bells.Bells).forEach(function(item) {
+    if (item == "_all") {
+      return
+    }
     if (bells.Bells[item].Enabled == true) { // Only interested in enabled bells
       var interval = parser.parseExpression(bells.Bells[item].Time);
       diff = moment().diff(interval.next())
@@ -420,7 +458,7 @@ function showTable() {
   });
 
   Object.keys(bells.Bells).forEach(function(item) {
-
+    if(item === "_all") { return }
     // Add details to the table
     table.push(
       [item, bells.Bells[item].Name, bells.Bells[item].Description, bells.Bells[item].Time, bells.Bells[item].File, bells.Bells[item].TriggerEmail.Enabled, bells.Bells[item].Enabled]
@@ -433,10 +471,10 @@ function showTable() {
 
 function c(text) {
   if (text == null) {
-    console.log("")
+    console.log("<br />")
   } else {
     text = moment().format(config.DateFormat) + " - " + text
-    console.log(text)
+    console.log(text + "<br />")
       // fs.appendFile(config.LogFile, text + "\r\n")
 
   }
