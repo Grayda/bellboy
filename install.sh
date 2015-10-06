0c204dafb522b665cea1ccedda825c13e8c7b898
David Gray <grayda@solidinc.org>
2015-10-06 13:58:25 +1100

    Updates to installer

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #! /bin/sh
 echo -e "\033[1mThis script will install everything necessary to run Bellboy on a Raspberry Pi"
-echo "Please only use this on a fresh install of Raspbpian, as it will install additional kernels, set passwords, startx on boot etc."
+echo "Please only use this on a fresh install of Raspbpian provided by the Raspberry Pi Foundation, as it will install additional kernels, set passwords, startx on boot etc."
 echo
-echo "To learn more, please visit http://github.com/Grayda/bellboy\033[0m"
-read -p "Press any key to continue or Ctrl+C to quit"
+echo -e "To learn more, please visit http://github.com/Grayda/bellboy\033[0m"
+read -p "Press any key to continue or Ctrl+C to quit" < /dev/tty
+echo "Updating system"
+sudo apt-get dist-upgrade
 echo "Preparing to install.."
 cd /home/pi
 echo "Downloading latest Node deb.."
@@ -46,16 +48,17 @@ echo -e "> User Password (\033[0;31m!IMPORTANT!\033[0m)"
 echo "> Hostname (under Advanced Options) (optional, but recommended)"
 echo "There is no need to enable Boot to Desktop. This will be done after you exit raspi-config"
 
-read -p "Press [Enter] to continue"
+read -p "Press [Enter] to continue" < /dev/tty
 sudo raspi-config
 
-# echo "Bellboy installation is complete. Next, we'll try and install the support for files for the Adafruit PiTFT 2.2\" screen"
-# echo -e "\033[0;31m This could take 20 minutes or more \033[0m"
-# read -p "If you do not have the PiTFT or else don't wish to install the files, press Ctrl+C now, otherwise, press any key to continue"
-# curl -SLs https://apt.adafruit.com/add-pin | sudo bash
-# sudo apt-get install raspberrypi-bootloader
-# sudo apt-get install adafruit-pitft-helper
-# sudo adafruit-pitft-helper -t 22
-# read -p "Installation complete. Please see https://goo.gl/WxMf9g for instructions on booting to X on the PiTFT. Press any key to reboot or Ctrl+C to exit"
+echo "Bellboy installation is complete. Next, we'll try and install the support for files for the Adafruit PiTFT 2.2\" screen"
+echo "When prompted, say no to setting the pin to power off"
+echo -e "\033[0;31m This could take 20 minutes or more \033[0m"
+read -p "If you do not have the PiTFT or else don't wish to install the files, press Ctrl+C now, otherwise, press any key to continue" < /dev/tty
+curl -SLs https://apt.adafruit.com/add-pin | sudo bash
+sudo apt-get install raspberrypi-bootloader
+sudo apt-get install adafruit-pitft-helper
+sudo adafruit-pitft-helper -t 22
+read -p "Installation complete. Please see https://goo.gl/WxMf9g for instructions on booting to X on the PiTFT. Press any key to reboot or Ctrl+C to exit"
 echo "Restarting the Pi. When it's booted, please visit the new hostname on port 8080 in your browser to begin"
 sudo reboot
