a24784fe1c33eaa059c6f36d77308d4e6c980525
David Gray <grayda@solidinc.org>
2015-10-08 19:09:36 +1100

    Authentication and more additions to the bell management page

--- addons/bellweb/routes/addedit.js
+++ addons/bellweb/routes/addedit.js
@@ -3,16 +3,25 @@ var router = express.Router();
 
 module.exports = function(passport, bellboy) {
 
+  /* GET login page. */
   router.get('/', function(req, res) {
+    if(req.isAuthenticated()) {
       // Display the Login page with any flash message, if any
       bellboy.modules["bellweb"].emit("pageloaded", req)
-      res.render('add/index.ejs', { isAuthenticated: req.isAuthenticated(), req: req });
+      res.render('add/index', { isAuthenticated: req.isAuthenticated(), req: req });
+    } else {
+      res.redirect("/login")
+    }
   });
 
   router.get('/:file', function(req, res) {
+    if(req.isAuthenticated()) {
       // Display the Login page with any flash message, if any
       bellboy.modules["bellweb"].emit("pageloaded", req)
       res.render('add/' + req.params.file, { isAuthenticated: req.isAuthenticated(), req: req });
+    } else {
+      res.redirect("/login")
+    }
   });
 
 
