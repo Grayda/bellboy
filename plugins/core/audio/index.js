1903afe8daa2bcfdf73c1fd52decd865d2389dd6
David Gray <grayda@solidinc.org>
2015-11-11 07:45:51 +1100

    Audio plugin, broken.

--- plugins/core/audio/index.js
+++ plugins/core/audio/index.js
@@ -1,23 +1,42 @@
 module.exports = function setup(options, imports, register) {
-    var _ = require("lodash")
+  var _ = require("lodash")
+  var validator = require("validator")
+  var cp = require("child_process")
+  var os = require("os");
 
-    imports.eventbus.on("trigger", function(bell) {
-      play(bell)
-    })
+  imports.eventbus.on("trigger", function(bell) {
+    play(bell)
+  })
 
-    register(null, {
-      audio: {
-        play: function(bell) {
-          try {
-            file = _.shuffle(bell.Actions.Audio.File)
-            this.playFile(file[0])
-          } catch(ex) {
-            imports.eventbus.error(ex)
-          }
-        },
-        playFile: function(file) {
+  register(null, {
+    audio: {
+      play: function(bell) {
+        try {
+          file = _.shuffle(bell.Actions.Audio.File)[0]
+          this.playFile(file)
+        } catch (ex) {
+          imports.eventbus.error(ex)
+        }
+      },
+      playFile: function(file, loop) {
+        if (typeof loop === "number") {
+          loop = "--loop " + loop
+        } else {
+          loop = ""
+        }
+        if (os.platform() !== "win32") {
+          console.log("Trying to play " + "mpg123 \"" + bellboy.__dirname + directory + file + "\" " + loop)
+          cp.exec("mpg123 " + loop + " \"" + bellboy.__dirname + directory + file + "\"", function(error, stdout, stderr) {
+            console.log(stdout || stderr || error)
+          })
+        } else {
+          console.log("Trying to play " + "\"" + __dirname + "/mpg123.exe\" " + loop + " \"" + bellboy.__dirname + directory + file + "\"")
+          cp.exec("\"" + __dirname + "/mpg123/mpg123.exe\" " + loop + " \"" + bellboy.__dirname + directory + file + "\" ", function(error, stdout, stderr) {
+            console.log(stdout || stderr || error)
+          })
 
-        },
-      }
-    });
+        }
+      },
+    }
+  });
 };
