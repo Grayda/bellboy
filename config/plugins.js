63cea11b1a108fca88d238f7f6be010351f4b749
David Gray <grayda@solidinc.org>
2015-11-23 12:22:57 +1100

    Cleaned up a bunch of files from the old project. Will restore later as necessary.
    Cleanup in aisle four!

--- config/plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [
-  { packagePath: "../plugins/core/eventbus"},
-  { packagePath: "../plugins/core/config", bellFile: __dirname + "/bells.json", configFile: __dirname + "/config.json" }
-]
