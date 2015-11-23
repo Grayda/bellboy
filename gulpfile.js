86f1a6ef4c0206c05c6d11dbd6d08feadced117e
David Gray <grayda@solidinc.org>
2015-11-23 12:18:35 +1100

    Updates to gulpfile and nodemon.json
    You can now call "gulp start" to start the server and nodemon.json now
    ignores bower_components

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,33 @@ var gulp = require("gulp")
 var dir = require("node-dir")
 var cp = require("child_process")
 var less = require('gulp-less');
+var nodemon = require('gulp-nodemon')
 var path = require('path');
-
-gulp.task('less', function (callback) {
-  console.log("Compiling LESS files to CSS")
-  return gulp.src('./plugins/core/web/public/stylesheets/*.less')
-    .pipe(less({
-      paths: [ path.join(__dirname, 'plugins', "core", "web", "public", "stylesheets", "includes") ]
-    }))
-    .pipe(gulp.dest(path.join(__dirname, 'plugins', "core", "web", "public", "stylesheets")));
-    callback()
-});
+var fs = require("fs")
 
 gulp.task("default", function(callback) {
-  console.log("List of available tasks: install-plugins")
+  console.log("List of available gulp tasks. Run with 'gulp [taskname]':")
+  console.log()
+  console.log("install          - Runs 'install-global', 'install-plugins', 'install-bower' and 'less'")
+  console.log("install-global   - Runs 'npm install' in the root folder, installs 'bower' globally, then installs 'less' globally")
+  console.log("install-plugins  - Recursively runs 'npm install' in every folder in '" + __dirname + "/plugins' (except 'node_modules' and 'bower_components')")
+  console.log("install-bower    - Runs 'bower install' in '" + __dirname + "/plugins/core/web'")
+  console.log("less             - Compiles all LESS files in " + __dirname + "/plugins/core/web/public/stylesheets'")
   callback()
 })
 
-gulp.task("install", function(callback) {
+gulp.task("start", ["less"], function(callback) {
+  nodemon({
+    script: 'index.js'
+  })
+})
+
+gulp.task("install", ["install-global", "install-plugins", "install-bower", "less"], function(callback) {
+  console.log("Installing all. This may take a while, please wait. ")
+  callback()
+})
+
+gulp.task("install-global", function(callback) {
   console.log("Installing dependencies")
   cp.execSync("npm install")
   console.log("Installing bower globally")
@@ -29,12 +38,12 @@ gulp.task("install", function(callback) {
   callback()
 })
 
-gulp.task("install-plugins", ["install"], function(callback) {
+gulp.task("install-plugins", function(callback) {
   console.log("Installing dependencies for plugins")
   dir.paths(__dirname + "/plugins", function(err, paths) {
     try {
       paths.dirs.forEach(function(item) {
-        if (item.indexOf("node_modules") > -1) {
+        if (item.indexOf("node_modules") > -1 || item.indexOf("bower_components") > -1) {
           return
         }
         console.log("Running 'npm install' in " + item)
@@ -43,16 +52,26 @@ gulp.task("install-plugins", ["install"], function(callback) {
         })
       })
     } catch (ex) {
-      console.log("Failed to run 'npm install' in " + item + ". Error was: " + ex)
+      console.log("Failed to run 'npm install'. Error was: " + ex)
     }
   })
   callback()
 })
 
-gulp.task("install-bower", ["install"], function(callback) {
+gulp.task("install-bower", function(callback) {
   console.log("Installing bower dependencies for web plugin")
   cp.execSync("bower install", {
     cwd: "./plugins/core/web"
   })
   callback()
+})
+
+gulp.task('less', function (callback) {
+  console.log("Compiling LESS files to CSS")
+  return gulp.src('./plugins/core/web/public/stylesheets/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'plugins', "core", "web", "public", "stylesheets", "includes") ]
+    }))
+    .pipe(gulp.dest(path.join(__dirname, 'plugins', "core", "web", "public", "stylesheets")));
+    callback()
 })
