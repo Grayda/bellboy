406e19ee144c37aac6087cd7773840c434600831
David Gray <grayda@solidinc.org>
2016-08-28 23:58:45 +1000

    Commented out these lines so the thing will actually run

--- plugins/users/index.js
+++ plugins/users/index.js
@@ -9,17 +9,17 @@ module.exports = function setup(options, imports, register) {
         groups: [],
         permissions: [],
         load: function() {
-          this.users = JSON.parse(fs.readFileSync(options.options.usersFile, 'utf8'))
-          this.groups = JSON.parse(fs.readFileSync(options.options.groupsFile, 'utf8'))
-          this.permissions = JSON.parse(fs.readFileSync(options.options.permissionsFile, 'utf8'))
+          // this.users = JSON.parse(fs.readFileSync(options.options.usersFile, 'utf8'))
+          // this.groups = JSON.parse(fs.readFileSync(options.options.groupsFile, 'utf8'))
+          // this.permissions = JSON.parse(fs.readFileSync(options.options.permissionsFile, 'utf8'))
 
         },
         // Obviously replace this with a function that handles security properly
         authenticate: function(key, permission) {
-          if(!_.includes(this.groups[this.users[key].groups].permissions, permission) && !_.includes(this.groups[this.users[key].groups].permissions, "all")) {
-            imports.eventbus.emit("users.authenticate.nopermission", { key: key, permission: permission })
-            return false
-          }
+          // if(!_.includes(this.groups[this.users[key].groups].permissions, permission) && !_.includes(this.groups[this.users[key].groups].permissions, "all")) {
+          //   imports.eventbus.emit("users.authenticate.nopermission", { key: key, permission: permission })
+          //   return false
+          // }
           if(key === "ABC123") {
             imports.eventbus.emit("users.authenticate.success", key)
             return true
