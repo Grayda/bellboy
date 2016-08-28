be4c25b79a53f0c422bd045e8eb810eab4292c7e
David Gray <grayda@solidinc.org>
2016-08-28 17:02:55 +1000

    install-plugins now skips folders with missing package.json

--- gulpfile.js
+++ gulpfile.js
@@ -3,49 +3,69 @@ var gutil = require('gulp-util');
 var bower = require('bower');
 var sh = require('shelljs');
 var cp = require("child_process")
-var dir = require("node-dir")
 var nodemon = require('gulp-nodemon')
 var path = require('path');
 var fs = require("fs")
 
+gulp.task('add-plugin', ['clone-plugin', 'install-plugins'], function() {
+
+})
+
+gulp.task('clone-plugin', function() {
+    if (!sh.which('git')) {
+        gutil.log(gutil.colors.red("git not installed!"))
+        process.exit(1)
+    } else if (typeof gutil.env.url === "undefined") {
+        gutil.log(gutil.colors.red("No plugin URL specified. use --url <github URL>"))
+    }
+
+    cp.execSync("git clone " + gutil.env.url, {
+        cwd: "./plugins"
+    })
+})
+
 // Goes through all the folders in the plugins directory, runs
 // npm install on each one, then adds it to plugins.json
 gulp.task('install-plugins', function() {
-  plugins = []
-  if(typeof gutil.env.path === "undefined") {
-    gutil.env.path = "./plugins"
-  }
-
-  try {
-    fs.statSync(gutil.env.path)
-  } catch(ex) {
-    gutil.log(gutil.colors.red("Plugin path not found! Check that it exists"))
-    process.exit(1)
-  }
-
-  dir.paths(__dirname + "/plugins", function(err, paths) {
-    try {
-      paths.dirs.forEach(function(item) {
-        if (item.indexOf("node_modules") > -1 || item.indexOf("bower_components") > -1) {
-          return
-        }
-        plugin = JSON.parse(fs.readFileSync(item + "/package.json"))
-        gutil.log("Installing " + gutil.colors.green(plugin.description))
-
-        plugins.push({
-          packagePath: item,
-          options: plugin.plugin.options || {}
-        })
-
-        cp.execSync("npm install", {
-         cwd: item
-        })
-      })
-      fs.writeFileSync(__dirname + "/plugins/plugins.json", JSON.stringify(plugins, null, "\t"))
-    } catch (ex) {
-      gutil.log(gutil.colors.red("Failed to run 'npm install'. Error was: ") + ex)
+    plugins = []
+    if (typeof gutil.env.path === "undefined") {
+        gutil.env.path = "./plugins"
     }
-  })
+
+    try {
+        fs.statSync(gutil.env.path)
+    } catch (ex) {
+        gutil.log(gutil.colors.red("Plugin path not found! Check that it exists"))
+        process.exit(1)
+    }
+    sh.cd("./plugins/")
+    paths = sh.ls("-A")
+    console.dir(paths)
+    try {
+        paths.forEach(function(item) {
+            if (item.indexOf("node_modules") > -1 || item.indexOf("bower_components") > -1 || item.indexOf(".git") > -1) {
+                return
+            } else if (sh.test("-f", item + "/package.json") == false) {
+                gutil.log(item + " doesn't contain a valid package.json, skipping")
+                return
+            }
+            plugin = JSON.parse(fs.readFileSync(item + "/package.json"))
+            gutil.log("Installing " + gutil.colors.green(plugin.description))
+
+            plugins.push({
+                packagePath: item,
+                options: plugin.plugin.options || {}
+            })
+
+            cp.execSync("npm install", {
+                cwd: item
+            })
+        })
+        fs.writeFileSync(__dirname + "/plugins/plugins.json", JSON.stringify(plugins, null, "\t"))
+    } catch (ex) {
+        gutil.log(gutil.colors.red("Failed to run 'npm install'. Error was: ") + ex)
+    }
+
 
 
 });
