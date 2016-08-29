ec22276f2082e1b952077887faa3340dbe6e415e
David Gray <grayda@solidinc.org>
2016-08-29 16:11:08 +1000

    Added wildcard to eventemitter2
    Doesn't actually work though

--- plugins/eventbus/index.js
+++ plugins/eventbus/index.js
@@ -1,11 +1,10 @@
 var EventEmitter2 = require("eventemitter2").EventEmitter2
 
 module.exports = function setup(options, imports, register) {
-    var emitter = new EventEmitter2()
+    var emitter = new EventEmitter2({ wildcard: true })
     eventbusObj = {
       pluginName: "Event Bus Plugin",
       pluginDescription: "Core plugin that provides inter-plugin communication",
-      emitter: emitter,
       emit: emitter.emit,
       on: emitter.on,
       error: function(err, module) {
