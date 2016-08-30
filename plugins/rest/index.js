fea02d9ae852303acd78a1b38d1fa6c834a898a2
David Gray <grayda@solidinc.org>
2016-08-30 16:09:06 +1000

    Formatting, plus error handling of locked bells

--- plugins/rest/index.js
+++ plugins/rest/index.js
@@ -1,94 +1,118 @@
 module.exports = function setup(options, imports, register) {
-    var restify = require("restify")
-    var passport = require("passport")
-    var Strategy = require('passport-http-bearer').Strategy;
+  var restify = require("restify")
+  var passport = require("passport")
+  var Strategy = require('passport-http-bearer').Strategy;
 
-    var restObj = {
-        pluginName: "REST Plugin",
-        pluginDescription: "Plugin that provides RESTful access to Bellboy",
-        restify: restify,
-        get: {
-            bells: function(req, res, next) {
-                if(typeof req.params.id === "undefined") {
-                  res.json(imports.bells.bells)
-                } else {
-                  res.json(imports.bells.get(req.params.id))
-                }
-
-                next()
-            },
-            nextbell: function(req, res, next) {
-                res.send(imports.scheduler.next())
-                next()
-            },
-            prevbell: function(req, res, next) {
-                res.json(imports.scheduler.prev())
-                next()
-            }
-        },
-        set: {
-            bell: function(req, res, next) {
-
-            },
-            enable: function(req, res, next) {
-                imports.bells.enable(req.params.id)
-                res.json(imports.bells.get(req.params.id))
-                next()
-            },
-            disable: function(req, res, next) {
-                imports.bells.disable(req.params.id)
-                res.json(imports.bells.get(req.params.id))
-                next()
-            },
-            enableAll: function(req, res, next) {
-                res.send(imports.bells.enableAll())
-            },
-            disableAll: function(req, res, next) {
-                res.send(imports.bells.disableAll())
-            }
-        },
-        trigger: function(req, res, next) {
-          res.send(imports.scheduler.trigger(req.params.id))
-
-        }
-    }
-
-
-    restObj.server = restify.createServer();
-
-
-    restObj.server.use(passport.initialize());
-
-    passport.use(new Strategy(
-      function(token, cb) {
-        success = imports.users.authenticate(token)
-        if(!success) {
-          return cb(false)
+  var restObj = {
+    pluginName: "REST Plugin",
+    pluginDescription: "Plugin that provides RESTful access to Bellboy",
+    restify: restify,
+    get: {
+      bells: function(req, res, next) {
+        if (typeof req.params.id === "undefined") {
+          res.json(imports.bells.bells)
         } else {
-          return cb(null, token);
+          res.json(imports.bells.get(req.params.id))
         }
 
-        })
-      )
+        next()
+      },
+      nextbell: function(req, res, next) {
+        res.send(imports.scheduler.next())
+        next()
+      },
+      prevbell: function(req, res, next) {
+        res.json(imports.scheduler.prev())
+        next()
+      }
+    },
+    set: {
+      bell: function(req, res, next) {
 
-    // The list of methods we can call. Each one is authenticated using Passport, and the relevant function is called above
-    // restObj.server.get('/bells', passport.authenticate('bearer', { session: false }), restObj.get.bells);
+      },
+      enable: function(req, res, next) {
+        if (imports.bells.enable(req.params.id) == false) {
+          res.send(423, new Error("Can't enable bell. Perhaps " + req.params.id + " is locked?"))
+        } else {
+          res.json(imports.bells.get(req.params.id))
+          next()
+        }
+      },
+      disable: function(req, res, next) {
+        if (imports.bells.disable(req.params.id) == false) {
+          res.send(423, new Error("Can't disable bell. Perhaps " + req.params.id + " is locked?"))
+        } else {
+          res.json(imports.bells.get(req.params.id))
+          next()
+        }
+      },
+      enableAll: function(req, res, next) {
+        res.send(imports.bells.enableAll())
+      },
+      disableAll: function(req, res, next) {
+        res.send(imports.bells.disableAll())
+      }
+    },
+    trigger: function(req, res, next) {
+      res.send(imports.scheduler.trigger(req.params.id))
+    }
+  }
 
-    restObj.server.get('/bells/next', passport.authenticate('bearer', { session: false, scope: ["read"] }), restObj.get.nextbell);
-    restObj.server.get('/bells/enable/all', passport.authenticate('bearer', { session: false }), restObj.set.enableAll);
-    restObj.server.get('/bells/disable/all', passport.authenticate('bearer', { session: false }), restObj.set.disableAll);
-    restObj.server.get('/bells/enable/:id', passport.authenticate('bearer', { session: false }), restObj.set.enable);
-    restObj.server.get('/bells/trigger/:id', passport.authenticate('bearer', { session: false }), restObj.trigger);
-    restObj.server.get('/bells/disable/:id', passport.authenticate('bearer', { session: false }), restObj.set.disable);
-    restObj.server.get('/bells/prev', passport.authenticate('bearer', { session: false }), restObj.get.prevbell);
-    restObj.server.get('/bells/:id', passport.authenticate('bearer', { session: false }), restObj.get.bells);
 
-    restObj.server.listen(9001, function() {
-        imports.logger.log("REST server started. Access it via " + restObj.server.url)
-    });
+  restObj.server = restify.createServer();
 
 
-    register(null, {
-        rest: restObj
-    })
+  restObj.server.use(passport.initialize());
+
+  passport.use(new Strategy(
+    function(token, cb) {
+      success = imports.users.authenticate(token)
+      if (!success) {
+        return cb(false)
+      } else {
+        return cb(null, token);
+      }
+
+    }))
+
+  // The list of methods we can call. Each one is authenticated using Passport, and the relevant function is called above
+  // restObj.server.get('/bells', passport.authenticate('bearer', { session: false }), restObj.get.bells);
+
+  restObj.server.get('/bells/next', passport.authenticate('bearer', {
+    session: false,
+    scope: ["read"]
+  }), restObj.get.nextbell);
+  restObj.server.get('/bells/enable/all', passport.authenticate('bearer', {
+    session: false
+  }), restObj.set.enableAll);
+  restObj.server.get('/bells/disable/all', passport.authenticate('bearer', {
+    session: false
+  }), restObj.set.disableAll);
+  restObj.server.get('/bells/enable/:id', passport.authenticate('bearer', {
+    session: false
+  }), restObj.set.enable);
+  restObj.server.get('/bells/trigger/:id', passport.authenticate('bearer', {
+    session: false
+  }), restObj.trigger);
+  restObj.server.get('/bells/disable/:id', passport.authenticate('bearer', {
+    session: false
+  }), restObj.set.disable);
+  restObj.server.get('/bells/prev', passport.authenticate('bearer', {
+    session: false
+  }), restObj.get.prevbell);
+  restObj.server.get('/bells/:id', passport.authenticate('bearer', {
+    session: false
+  }), restObj.get.bells);
+  restObj.server.get('/bells', passport.authenticate('bearer', {
+    session: false
+  }), restObj.get.bells);
+
+  restObj.server.listen(9001, function() {
+    imports.logger.log("REST server started. Access it via " + restObj.server.url)
+  });
+
+
+  register(null, {
+    rest: restObj
+  })
 }
