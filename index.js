4a471c64f1ee90db028ba8c4239b7c1e76fe61c5
David Gray <grayda@solidinc.org>
2016-09-02 15:11:22 +1000

    Welcome messages for fun

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var fs = require("fs")
 var architect = require("architect")
 var cfonts = require("cfonts")
+var _ = require("lodash")
+var messages = [
+  "reticulating spines",
+  "hurting llamas",
+  "installing dingdongs",
+  "giving jobs to others",
+  "delagating tasks to Architect",
+  "giving standing orders to plugins",
+  "starting micromanagement service"
+]
+
 
 console.log()
 console.log()
@@ -9,13 +20,17 @@ cfonts.say(' bellboy', {
     font: 'block',        //define the font face
     align: 'left',        //define text alignment
     colors: ["white", "black"], //define all colors
-    background: 'black', //define the background color    letterSpacing: 1,     //define letter spacing
+    background: 'black', //define the background color
+    letterSpacing: 1,     //define letter spacing
     lineHeight: 1,        //define the line height
     space: false,          //define if the output text should have empty lines on top and on the bottom
     maxLength: '0'        //define how many character can be on one line
 })
 console.log("        The Bell Timer System")
 console.log()
+console.log()
+
+console.log("Please wait, " + _.sample(messages))
 console.log()
 
 // A list of plugins we'd like to load. Some of them require options, so be sure to check the plugin for what options are required
