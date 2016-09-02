e04b49a64a3e2250f1a334b3786f1fa1cb47717c
David Gray <grayda@solidinc.org>
2016-09-02 12:17:41 +1000

    Tweaks for cloning

--- install.sh
+++ install.sh
@@ -2,23 +2,33 @@
 
 # Start epiphany. Use this to authenticate to the internet
 epiphany
+
 # Install git
 sudo apt-get install git
+
+# make and cd into the bellboy directory
+mkdir ~/bellboy
+cd ~/bellboy
+
 # Clone Bellboy
-git clone http://github.com/grayda/bellboy ~/
+git clone http://github.com/grayda/bellboy ~/bellboy
+
 # Get the latest version of node
 curl -skL https://deb.nodesource.com/setup_6.x | sudo -E bash
+
 # Install everything
 sudo npm install -g gulp nodemon
-# cd into the bellboy directory
-cd ~/bellboy
+
 # Install stuff for bellboy
 npm install
+
 # Run the gulp task to set stuff up
 gulp install-plugins
+
 # Set your hostname
 echo Installation complete. Set your hostname to the following using sudoedit /etc/hostname:
 echo
+echo -n bellboy_
 date | md5sum | head -c 6 ; echo
 echo
 echo nodemon will start in 10 seconds
